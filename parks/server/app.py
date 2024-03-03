411b4d97cdc7cd8a627d90a4e4a0419d0ad6b69b
Alex Bailey <alex.bailey@verivin.com>
2024-03-03 10:33:07 +0000

    Use templates and static files to host frontend

--- parks/server/app.py
+++ parks/server/app.py
@@ -1,29 +1,18 @@
-# from database import session
-
 import logging
+import os
 from importlib.metadata import version
-
 from fastapi import FastAPI, Request, Response
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.middleware.gzip import GZipMiddleware
-# from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
 from fastapi.responses import HTMLResponse
-import uvicorn
-
-# from config import config
-
-__version__ = version("parks")
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 from .uvicorn_logging import setup_logging
+from .routers import v1
 
-logger = logging.getLogger("MainApp")
-
+__version__ = version("parks")
 app = FastAPI(title="PARKS API", version=__version__)
-
-### MIDDLEWARE ###
-origins = [
-    "http://localhost:8080",
-]
 
 
 # REF: https://github.com/tiangolo/fastapi/issues/775
@@ -31,17 +20,19 @@ async def catch_exceptions_middleware(request: Request, call_next):
     try:
         return await call_next(request)
     except Exception as e:
-        # logging.exception(e)
+        logging.exception(e)
         return Response("Internal server error", status_code=500)
 
 
 # This should come before CORSMiddleware is added
 app.middleware("http")(catch_exceptions_middleware)
 
-# add CORS origins
+# MIDDLEWARE
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=[
+        "http://localhost:8080"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -49,30 +40,30 @@ app.add_middleware(
 
 app.add_middleware(GZipMiddleware, minimum_size=1000)
 
-
-@app.get("/", response_class=HTMLResponse)
-async def root():
-    """
-
-    Returns:
-        A block of HTML to use as the root containing the title and the version
-
-    """
-    return f"""
-    <html>
-        <head>
-            <title>{app.title}</title>
-        </head>
-        <body>
-            <h1>{app.title}</h1>
-            <i>Version: {__version__}</i>
-        </body>
-    </html>
-    """
+logger = logging.getLogger("MainApp")
 
 
-### ROUTERS ###
-from .routers import v1
+@app.on_event("startup")
+async def startup():
+    setup_logging()
 
+
+project_dir = os.path.dirname(__file__)
+template_dir = os.path.join(project_dir, "frontend/templates")
+static_dir = os.path.join(project_dir, "frontend/static")
+templates = Jinja2Templates(directory=template_dir)
+app.mount("/static", StaticFiles(directory=static_dir), name="static")
+
+
+@app.get('/')
+def index(request: Request):
+    return templates.TemplateResponse("index.html", {
+        "version": __version__,
+        "title": "PARKS",
+        "request": request
+    })
+
+
+# ROUTERS
 app.include_router(v1.router, prefix="/v1")
 
