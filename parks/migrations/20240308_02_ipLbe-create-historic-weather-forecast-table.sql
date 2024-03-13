0351fe1e617a5ae13f412625f1bd590e1415f9a2
Alex Bailey <alex.bailey@verivin.com>
2024-03-13 16:45:44 +0000

    Create tables for weather data

---
+++ parks/migrations/20240308_02_ipLbe-create-historic-weather-forecast-table.sql
@@ -0,0 +1,41 @@
+-- create-historic-weather-forecast-table
+-- depends: 20240308_01_kKWih-create-trigger-to-extract-carpark-spaces-from-server-response
+
+-- ######################### CREATE TABLES #########################
+
+-- Create weather_location table
+CREATE TABLE weather_location
+(
+    id                              UUID                        DEFAULT UUID_GENERATE_V4()  NOT NULL,
+    name                            TEXT                                                    NOT NULL,
+    lat                             REAL                                                    NOT NULL,
+    lon                             REAL                                                    NOT NULL,
+    timezone                        TEXT                                                    NOT NULL,
+    timezone_offset                 INTEGER                                                 NOT NULL,
+)
+
+-- Create weather_forecast table
+CREATE TABLE weather_forecast
+(
+    weather_location_id             UUID                                                    NOT NULL,
+    time                            TIMESTAMP WITH TIME ZONE                                NOT NULL,
+    data                            JSONB                                                   NOT NULL
+);
+
+-- ######################### ADD KEYS AND INDEXES #########################
+
+-- Keys and indexes for weather_location
+
+-- Make id the primary key
+ALTER TABLE weather_location ADD PRIMARY KEY (id);
+
+-- Keys and indexes for weather_forecast
+
+-- Make weather_location_id and time the primary key
+ALTER TABLE weather_forecast ADD PRIMARY KEY (weather_location_id, time);
+
+-- Add foreign key to the weather_location table
+ALTER TABLE weather_forecast ADD FOREIGN KEY (weather_location_id) REFERENCES weather_location(id) ON DELETE SET NULL;
+
+-- Add index to time column
+CREATE INDEX ON weather_forecast (time);
