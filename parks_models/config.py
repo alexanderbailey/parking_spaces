8ead5f56dd15daf06a8918777240e86b4628e17b
Alex Bailey <alex.bailey@verivin.com>
2025-08-17 22:36:50 +0100

    Update frontend to include charles street carpark

---
+++ parks_models/config.py
@@ -0,0 +1,13 @@
+from dotenv import load_dotenv, find_dotenv
+from os import getenv
+load_dotenv(find_dotenv())
+
+db_host = getenv("DB_HOST")
+db_port = getenv("DB_PORT")
+db_name = getenv("DB_NAME")
+db_user = getenv("DB_USER")
+db_pass = getenv("DB_PASS")
+db_url = f"postgresql://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}"
+
+cache_dir = getenv("CACHE_DIR")
+cache_format = getenv("CACHE_FORMAT", "parquet")
